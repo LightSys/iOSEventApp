78600d079c0dc0ffc74414bb7204bc2b343a6c42
Kaitlyn Morris <kaitlyn@mokido.net>
2018-03-06 14:11:43 -0700

    Committing!

---
+++ iOSEventApp/File.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  iOSEventApp
+//
+//  Created by Kaitlyn Morris on 3/5/18.
+//  Copyright © 2018 LightSys. All rights reserved.
+//
+
+import UIKit
+
+class SideBarViewController: UIViewController, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+}
---
+++ iOSEventApp/SidebarTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SidebarTableViewCell.swift
+//  iOSEventApp
+//
+//  Created by Kaitlyn Morris on 3/5/18.
+//  Copyright © 2018 LightSys. All rights reserved.
+//
+
+import UIKit
+
+class SidebarTableViewCell: UITableViewCell {
+    
+    
+}
